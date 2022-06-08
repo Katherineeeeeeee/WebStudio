3acf1ec28108f93ccf8846acd2ba2cb223912ad3
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-06-08 13:50:37 +0300

    Added styles

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,15 @@
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
-  
-    menuBtnRef.addEventListener('click', () => {
-      const expanded =
-        menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-  
-      menuBtnRef.classList.toggle('is-active');
-      menuBtnRef.setAttribute('aria-expanded', !expanded);
-  
+  const menuBtnRef = document.querySelectorAll('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.forEach(btn =>
+    btn.addEventListener('click', () => {
+      const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+
+      btn.classList.toggle('is-active');
+      btn.setAttribute('aria-expanded', !expanded);
+
       mobileMenuRef.classList.toggle('is-open');
-    });
-  })();
-
-
+    })
+  );
+})();
